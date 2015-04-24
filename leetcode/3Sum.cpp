2a17431eee4e41e195a35e0c43d6b2db9168054f
yuanzhiyuan <707699544@qq.com>
2015-04-25 00:37:11 +0800

    3sum

---
+++ leetcode/3Sum.cpp
@@ -0,0 +1,36 @@
+// LeetCode, 3Sum
+// 先排序，然后左右夹逼，注意跳过重复的数，时间复杂度O(n^2)，空间复杂度O(1)
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& num) {
+		vector<vector<int>> result;
+		if (num.size() < 3) return result;
+		sort(num.begin(), num.end());
+		const int target = 0;
+		auto last = num.end();
+		for (auto i = num.begin(); i < last-2; ++i) {
+			auto j = i+1;
+			if (i > num.begin() && *i == *(i-1)) continue;
+			auto k = last-1;
+			while (j < k) {
+				if (*i + *j + *k < target) {
+					++j;
+					while(*j == *(j - 1) && j < k) ++j;
+				} else if (*i + *j + *k > target) {
+					--k;
+					while(*k == *(k + 1) && j < k) --k;
+				} else {
+					result.push_back({ *i, *j, *k });
+					++j;
+					--k;
+					while(*j == *(j - 1) && *k == *(k + 1) && j < k) {
+						++j;
+						--k;
+					};
+				}
+			}
+		}
+		return result;
+	}
+};
+
